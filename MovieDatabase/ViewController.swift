f79190a21e77bbb7f88c40c1cb0f40fa0969d3b8
Mikhail Tiranov <m.tiranov@bitlica.com>
2023-06-23 14:37:49 +0200

    Initial Commit

---
+++ MovieDatabase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieDatabase
+//
+//  Created by Mihail Tiranov on 23.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
